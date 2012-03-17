3010df2cb9a2729ce29366f886a85a44bc23bfc2
Josh Powell <jpowell@rocketfuel.com>
2012-03-16 22:57:41 -0700

    add selector for target
    change array param to options object

--- eztreez.js
+++ eztreez.js
@@ -1,15 +1,21 @@
 (function ($) {
   $.fn.ezTreez = function (options) {
     return this.each(function () {
-      var children = $(this).find('> tbody > tr, > li, > div');
+      var children = $(this).find('> tbody > tr, > li, > div'),
+          hierarchy = options.hierarchy,
+          click_element_selector = options.click_element_selector;
 
-      $.each(options, function (index) {
-        var stop_elements_selector = '.' + options.slice(0, index + 1).join(',.'),
-            current_class = options[index];
+      $.each(hierarchy, function (index) {
+        var stop_elements_selector = '.' + hierarchy.slice(0, index + 1).join(',.'),
+            current_class = hierarchy[index];
 
-        children.filter('.' + current_class).click(function (e) {
-          $(this).nextUntil(stop_elements_selector).toggle();
+        children.filter('.' + current_class).each(function () {
+          var branch = $(this);
+          branch.find(click_element_selector).click(function (e) {
+            branch.nextUntil(stop_elements_selector).toggle();
+          });
         });
+        
       });
     
     });
