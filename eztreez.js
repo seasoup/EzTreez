08b09132e88221b6d86f1ce16aa1fb560de5182e
Josh Powell <jpowell@rocketfuel.com>
2012-03-19 14:37:05 -0700

    small optimizations

--- eztreez.js
+++ eztreez.js
@@ -2,13 +2,14 @@
   $.fn.ezTreez = function (options) {
     var hierarchy = options.hierarchy,
         hierarchy_classes = '.' + hierarchy.join(',.'),
-        click_element_selector = options.click_element_selector,
-        stop_elements_selector = $.map(hierarchy, function (a,index) {
+        click_element_selector   = options.click_element_selector,
+        expandable_rows_selector = '.' + hierarchy.slice(0, hierarchy.length-1).join(',.'),
+        stop_elements_selector   = $.map(hierarchy, function (a, index) {
           return '.' + hierarchy.slice(0, index + 1).join(',.');
         });
         
     return this.each(function () {
-      var children = $(this).find('> tbody > tr, > li, > div');
+      var children = $(this).find('> tbody > tr, > li, > div').filter(expandable_rows_selector);
           
       $.each(hierarchy, function (index) {
         var current_class = hierarchy[index],
