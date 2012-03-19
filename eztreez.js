7cc5950e747f1df5437eaa00689fd9f5a75463ee
Josh Powell <jpowell@rocketfuel.com>
2012-03-19 14:25:29 -0700

    delegate events, speed improvements
    also add click link [+] to all examples

--- eztreez.js
+++ eztreez.js
@@ -1,23 +1,25 @@
 (function ($) {
   $.fn.ezTreez = function (options) {
+    var hierarchy = options.hierarchy,
+        hierarchy_classes = '.' + hierarchy.join(',.'),
+        click_element_selector   = options.click_element_selector,
+        expandable_rows_selector = '.' + hierarchy.slice(0, hierarchy.length-1).join(',.'),
+        stop_elements_selector   = $.map(hierarchy, function (a, index) {
+          return '.' + hierarchy.slice(0, index + 1).join(',.');
+        });
+        
     return this.each(function () {
-      var children = $(this).find('> tbody > tr, > li, > div'),
-          hierarchy = options.hierarchy,
-          click_element_selector = options.click_element_selector;
-
+      var children = $(this).find('> tbody > tr, > li, > div').filter(expandable_rows_selector);
+          
       $.each(hierarchy, function (index) {
-        var stop_elements_selector = '.' + hierarchy.slice(0, index + 1).join(',.'),
-            current_class = hierarchy[index];
-
-        children.filter('.' + current_class).each(function () {
-          var branch = $(this);
-          branch.find(click_element_selector).click(function (e) {
-            branch.nextUntil(stop_elements_selector).toggle();
-          });
+        var current_class = hierarchy[index],
+            index = index;
+            
+        children.filter('.' + current_class).on('click', click_element_selector, function () {
+          $(this).closest(hierarchy_classes).nextUntil(stop_elements_selector[index]).toggle();
         });
         
       });
-    
     });
   };
 })(jQuery);
